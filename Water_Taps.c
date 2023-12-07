3f3f9bc4754705602a6a9d615132926cd3491999
bhavyasatyasri <147791871+bhavyasatyasri@users.noreply.github.com>
2023-12-07 11:57:04 +0530

    #include<stdio.h> int add(int,int); int main() {     int x,y;     scanf("%d%d",&x,&y);     int res=add(x,y);     printf("%d",res); } int add(int x,int y) {     int c;     c=(x*y)/(x+y);     return c; }

---
+++ Water_Taps.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int add(int,int);
+int main()
+{
+    int x,y;
+    scanf("%d%d",&x,&y);
+    int res=add(x,y);
+    printf("%d",res);
+}
+int add(int x,int y)
+{
+    int c;
+    c=(x*y)/(x+y);
+    return c;
+}
