641c2672ff534026ad515cfe35edf23549300056
bhavyasatyasri <147791871+bhavyasatyasri@users.noreply.github.com>
2024-02-27 23:47:55 -0800

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             if(i==j || i+j==n+1)             {                 printf("%c",64+i);             }             else             {                 printf(" ");             }
            }
            printf("
    ");
           
        }
    }

--- Pattern_8.5.c
+++ Pattern_8.5.c
@@ -5,7 +5,7 @@ int main()
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        for(j=n;j>=1;j--)
+        for(j=1;j<=n;j++)
         {
             if(i==j || i+j==n+1)
             {
@@ -15,8 +15,10 @@ int main()
             {
                 printf(" ");
             }
+            
         }
         printf("
 ");
+       
     }
 }
